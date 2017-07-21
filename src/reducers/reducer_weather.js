dd69760b8bffa74169893dbe9f5ba13c45d0f3f3
limozaza <zakboite@gmail.com>
2017-07-21 23:53:57 +0200

    Add Reducer

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,10 @@
-export default (state=null, action) => {
-    console.log('action received ',action);
-    return state;
+import {FETCH_WEATHER} from '../actions/index';
+
+export default (state=[], action) => {
+    switch (action.type) {
+        case FETCH_WEATHER:
+            return [action.payload.data, ...state];
+        default:
+            return state;
+    }
 } 
